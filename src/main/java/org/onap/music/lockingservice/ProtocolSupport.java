6f450be557a7c25d9aff9c849bc5f64b1457c9ca
Arundathi Patil <arundpil@in.ibm.com>
2018-09-03 15:54:13 +0530

    ProtocolSupport.java: fixed sonar issue
    Fixed sonar code-smella/issues accross this file
    
    Issue-ID: MUSIC-124
    Change-Id: Iebfaae30d941761ffbe59920cf6301cf22daae44
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- src/main/java/org/onap/music/lockingservice/ProtocolSupport.java
+++ src/main/java/org/onap/music/lockingservice/ProtocolSupport.java
@@ -174,8 +174,10 @@ class ProtocolSupport {
                 }
             });
         } catch (KeeperException e) {
+        	logger.error("Error", e);
         	logger.error(EELFLoggerDelegate.errorLogger, e.getMessage(),AppMessages.KEEPERERROR, ErrorSeverity.ERROR, ErrorTypes.LOCKINGERROR);
         } catch (InterruptedException e) {
+        	logger.error("Error", e);
         	logger.error(EELFLoggerDelegate.errorLogger, e.getMessage(),AppMessages.EXECUTIONINTERRUPTED, ErrorSeverity.ERROR, ErrorTypes.LOCKINGERROR);
         }
     }
