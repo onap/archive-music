af043d24e25ac7f6777a58d12d38c9f87d89f9bd
Sooriyaa <soponraj@in.ibm.com>
2018-09-04 15:34:31 +0530

    Remove the unused import
    Remove the unused import
    
    Issue-ID: MUSIC-128
    Change-Id: Ic12deb0959761f2878fdf20b71e012ee3a0bac2e
    Signed-off-by: Sooriyaa <soponraj@in.ibm.com>

--- src/main/java/org/onap/music/datastore/jsonobjects/JsonNotifyClientResponse.java
+++ src/main/java/org/onap/music/datastore/jsonobjects/JsonNotifyClientResponse.java
@@ -22,12 +22,10 @@
 package org.onap.music.datastore.jsonobjects;
 
 import java.io.Serializable;
-import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "JsonNotifyClientResponse", description = "Json model for callback")
 @JsonIgnoreProperties(ignoreUnknown = true)
