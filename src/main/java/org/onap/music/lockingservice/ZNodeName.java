a4fe2f2cb76b373d473c52f2e61ca8ccfaf82536
Sandeep J <sandeejh@in.ibm.com>
2019-01-11 11:10:25 +0530

    fixed sonar issue in ZNodeName.java
    fixed sonar issue
    
    Issue-ID: MUSIC-182
    Change-Id: I5079c24e9f9342e2e6634e066c51c8eb0a03207b
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- src/main/java/org/onap/music/lockingservice/ZNodeName.java
+++ src/main/java/org/onap/music/lockingservice/ZNodeName.java
@@ -4,6 +4,8 @@
  * ===================================================================
  *  Copyright (c) 2017 AT&T Intellectual Property
  * ===================================================================
+ * Modifications Copyright (c) 2018 IBM
+ * ===================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -59,7 +61,7 @@ class ZNodeName implements Comparable<ZNodeName> {
 
     @Override
     public String toString() {
-        return name.toString();
+        return name;
     }
 
     @Override
