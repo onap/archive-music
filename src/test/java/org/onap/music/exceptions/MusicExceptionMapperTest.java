0874292bf8ff6d4c1cb2e90821683cee56ef2fa8
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-01-18 17:12:55 +0530

    MusicExceptionMapper - Junit test cases
    MusicExceptionMapper - Junit test cases
    
    Issue-ID: MUSIC-273
    Change-Id: I55d23c353bf291c21c43e5df5d41aff7e4df698f
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

---
+++ src/test/java/org/onap/music/exceptions/MusicExceptionMapperTest.java
@@ -0,0 +1,60 @@
+/*
+ * ============LICENSE_START==========================================
+ * org.onap.music
+ * ===================================================================
+ * Copyright (c) 2019 IBM Intellectual Property
+ * ===================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * ============LICENSE_END=============================================
+ * ====================================================================
+ */
+
+package org.onap.music.exceptions;
+
+import org.codehaus.jackson.map.exc.UnrecognizedPropertyException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.ws.rs.core.Response;
+import java.io.EOFException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(PowerMockRunner.class)
+public class MusicExceptionMapperTest {
+
+    @Test
+    public void testToResponse() {
+        MusicExceptionMapper musicExceptionMapper = new MusicExceptionMapper();
+        UnrecognizedPropertyException unrecognizedPropertyException = PowerMockito.mock(UnrecognizedPropertyException.class);
+        Response response = musicExceptionMapper.toResponse(unrecognizedPropertyException);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        assertTrue(((Map)response.getEntity()).get("error").toString().startsWith("Unknown field :"));
+
+        EOFException eofException = PowerMockito.mock(EOFException.class);
+        response = musicExceptionMapper.toResponse(eofException);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        assertTrue(((Map)response.getEntity()).get("error").toString().equals("Request body cannot be empty".trim()));
+
+        IllegalArgumentException illegalArgumentException = PowerMockito.mock(IllegalArgumentException.class);
+        PowerMockito.when(illegalArgumentException.getMessage()).thenReturn("ERROR MSG");
+        response = musicExceptionMapper.toResponse(illegalArgumentException);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        assertTrue(((Map)response.getEntity()).get("error").toString().equals("ERROR MSG".trim()));
+    }
+}
