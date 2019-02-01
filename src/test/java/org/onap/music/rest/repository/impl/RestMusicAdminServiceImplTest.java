3ea0b588826bfa6969855d645beba8885a541ff6
Chandan Ghosh <cghosh12@in.ibm.com>
2019-02-01 15:18:20 +0530

    Added testcase for service layer RestAdmin api
    Added testcase for service layer getOnboardedInfoSearch method
    
    Issue-ID: MUSIC-305
    Change-Id: If5ebc6969f3a4672b79e389147ec90de578596b0
    Signed-off-by: Chandan Ghosh <cghosh12@in.ibm.com>

---
+++ src/test/java/org/onap/music/rest/repository/impl/RestMusicAdminServiceImplTest.java
@@ -0,0 +1,91 @@
+/*
+ * ============LICENSE_START==========================================
+ * org.onap.music
+ * ===================================================================
+ *  Copyright (c) 2017 IBM Intellectual Property
+ * ===================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ * ============LICENSE_END=============================================
+ * ====================================================================
+ */
+package org.onap.music.rest.repository.impl;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.music.datastore.jsonobjects.JsonOnboard;
+import org.onap.music.main.CachingUtil;
+import org.onap.music.rest.repository.RestMusicAdminRepository;
+import org.onap.music.rest.service.RestMusicAdminService;
+import org.onap.music.rest.service.impl.RestMusicAdminServiceImpl;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestMusicAdminServiceImplTest {
+	
+	@InjectMocks
+	RestMusicAdminServiceImpl restMusicAdminServiceImpl;
+	
+	@Spy
+	RestMusicAdminRepository restMusicAdminRepository;
+	
+	@Mock
+	Iterator<Row> it;
+	
+	@Mock
+	ResultSet rs;
+	JsonOnboard jsonObj=new JsonOnboard();
+	
+	
+	@Before
+	public void setup() {
+		jsonObj.setAid("AID123");
+		jsonObj.setAppname("AppName");
+		jsonObj.setIsAAF("false");
+		jsonObj.setPassword("1234");
+		jsonObj.setUserId("user123");
+	}
+	
+	/*@Test
+	@Ignore
+	public void createLockReferenceTest() throws Exception {
+		Mockito.when(restMusicAdminRepository.getUuidFromKeySpaceMasterUsingAppName("AppName")).thenReturn(rs);
+		Mockito.when(restMusicAdminRepository.insertValuesIntoKeySpaceMaster("1234", "AppName", "user123", "false", "1234")).thenReturn("SUCCESS");
+		restMusicAdminServiceImpl.onboardAppWithMusic(jsonObj);
+	}*/
+	
+	@Test
+	public void getOnboardedInfoSearchTest() throws Exception {
+		Mockito.when(rs.iterator()).thenReturn(it);
+		Mockito.when(restMusicAdminRepository.fetchOnboardedInfoSearch("AppName", "AID123", "false")).thenReturn(rs);
+		Assert.assertNotNull(restMusicAdminServiceImpl.getOnboardedInfoSearch(jsonObj));
+	}
+}
