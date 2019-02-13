35320e7775e8ecb0962278b0ca2baf3ac8408372
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-02-13 21:51:10 +0530

    CadiAuthFilter.java - Sonar Fixes
    CadiAuthFilter.java - Sonar Fixes
    
    Issue-ID: MUSIC-331
    Change-Id: Ibdda5120cbda6ee056e2076b34c134927dc3102d
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- src/main/java/org/onap/music/CadiAuthFilter.java
+++ src/main/java/org/onap/music/CadiAuthFilter.java
@@ -3,6 +3,8 @@
  * org.onap.music
  * ===================================================================
  *  Copyright (c) 2017 AT&T Intellectual Property
+ *
+ *  Modifications Copyright (C) 2019 IBM.
  * ===================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -21,22 +23,14 @@
  */
 
 package org.onap.music;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.onap.aaf.cadi.Access;
 import org.onap.aaf.cadi.CadiWrap;
 import org.onap.aaf.cadi.Permission;
 import org.onap.aaf.cadi.PropAccess;
@@ -45,67 +39,17 @@ import org.onap.aaf.cadi.filter.CadiFilter;
 
 public class CadiAuthFilter extends CadiFilter {
 
-    private static String include_url_endpoints ="";
-    private static String exclude_url_endpoints = "";
-    public static final String AUTHORIZATION = "Authorization";
-    
     public CadiAuthFilter(boolean init, PropAccess access) throws ServletException {
         super(true, access);
     }
-    
+
+    @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         super.init(filterConfig);
-        include_url_endpoints = filterConfig.getInitParameter("include_url_endpoints");
-        exclude_url_endpoints = filterConfig.getInitParameter("exclude_url_endpoints");
     }
 
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        
-        super.doFilter(request, response, chain);
-    }
-
-
-    private String getUrl(ServletRequest request) {
-        String path = "";
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length() + 1);
-        return path;
-    }
-
-
-    private boolean matchPattern(String requestedPath, String includeUrl) {
-        includeUrl = includeUrl.substring(1);
-        String[] path = requestedPath.split("/");
-        if (path.length > 1) {
-            String[] roleFunctionArray = includeUrl.split("/");
-            boolean match = true;
-            for (int i = 0; i < roleFunctionArray.length; i++) {
-                if (match) {
-                    if (!roleFunctionArray[i].equals("*")) {
-                        Pattern p = Pattern.compile(Pattern.quote(path[i]), Pattern.CASE_INSENSITIVE);
-                        Matcher m = p.matcher(roleFunctionArray[i]);
-                        match = m.matches();
-                    } else if (roleFunctionArray[i].equals("*")) {
-                        match = true;
-                    }
-
-                }
-            }
-            if (match)
-                return match;
-        } else {
-            if (requestedPath.matches(includeUrl))
-                return true;
-            else if (includeUrl.equals("*"))
-                return true;
-        }
-        return false;
-    }
-    
-
-    public static List<AAFPermission> getAAFPermissions(HttpServletRequest request) { 
+    public static List<AAFPermission> getAAFPermissions(HttpServletRequest request) {
         CadiWrap wrapReq = (CadiWrap) request; 
         List<Permission> perms = wrapReq.getPermissions(wrapReq.getUserPrincipal()); 
         List<AAFPermission> aafPermsList = new ArrayList<>(); 
