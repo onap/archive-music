ea7350c99c35ed2af734969804b4539d8f2d6944
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-11-19 18:54:31 +0530

    ErrorTypes.java-junits
    Issue-ID: OPTFRA-578
    Change-Id: I59363871a2948a86895a0de774f16af00b1a9d60
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

---
+++ music-core/src/test/java/org/onap/music/eelf/logging/format/ErrorTypesTest.java
@@ -0,0 +1,48 @@
+/*
+ * ============LICENSE_START==========================================
+ * org.onap.music
+ * ===================================================================
+ *  Copyright (c) 2017 AT&T Intellectual Property
+ *  Modifications Copyright (C) 2019 IBM 
+ * ===================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ * ============LICENSE_END=============================================
+ * ====================================================================
+ */
+
+package org.onap.music.eelf.logging.format;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ErrorTypesTest {
+
+	@Test
+	public void Test1()
+	{
+		assertEquals("CONNECTIONERROR",ErrorTypes.CONNECTIONERROR.name());
+		assertEquals("SESSIONEXPIRED",ErrorTypes.SESSIONEXPIRED.name());
+		assertEquals("AUTHENTICATIONERROR",ErrorTypes.AUTHENTICATIONERROR.name());
+		assertEquals("CACHEERROR",ErrorTypes.CACHEERROR.name());
+		assertEquals("SERVICEUNAVAILABLE",ErrorTypes.SERVICEUNAVAILABLE.name());
+		assertEquals("QUERYERROR",ErrorTypes.QUERYERROR.name());
+		assertEquals("DATAERROR",ErrorTypes.DATAERROR.name());
+		assertEquals("GENERALSERVICEERROR",ErrorTypes.GENERALSERVICEERROR.name());
+		assertEquals("MUSICSERVICEERROR",ErrorTypes.MUSICSERVICEERROR.name());
+		assertEquals("LOCKINGERROR",ErrorTypes.LOCKINGERROR.name());
+		assertEquals("UNKNOWN",ErrorTypes.UNKNOWN.name());
+		
+	}
+}
